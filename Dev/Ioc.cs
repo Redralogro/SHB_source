091ce6abc013c88bea2d10969b90aff7f4e16ff9
Long Vu <redralogro@gmail.com>
2021-01-11 17:01:53 +0700

    Add .sln file and Ioc Research

---
+++ Dev/Ioc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dev
+{
+    public class Ioc
+    {
+       public  void Main(){
+           Console.WriteLine("HÚ le");
+       } 
+    }
+}
--- Dev/Program.cs
+++ Dev/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-
 namespace Dev
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args){
+        // {   int i ;
+        //     int j;
+
+        //     Console.Write("Enter First Name:");
+        //          i = Int16.Parse(Console.ReadLine());
+        //     Console.Write(i);
+        Ioc test = Factory.getObject();
+        test.Main();
+        }
+        // static void Main(string[] args){
+        // // {   int i ;
+        // //     int j;
+
+        // //     Console.Write("Enter First Name:");
+        // //          i = Int16.Parse(Console.ReadLine());
+        // //     Console.Write(i);
+        // Ioc test = new Ioc();
+        // test.Main();
+        //}
+    }
+    public class Factory {
+        public static Ioc getObject(){
+            return  new Ioc();
         }
     }
 }
