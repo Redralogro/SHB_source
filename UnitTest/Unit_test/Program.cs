5f4c17febf6cedc7d940e29def1331d5981c112d
Long Vu <redralogro@gmail.com>
2021-01-29 16:36:38 +0700

    Unit test init

---
+++ UnitTest/Unit_test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program
+{
+    public class UnitTest
+    {
+        public static void Main(string[] args) {
+
+            printf("Hello World ");
+        }
+    }
+}   
